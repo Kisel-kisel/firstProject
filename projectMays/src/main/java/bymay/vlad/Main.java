e80ab2eda98b1954502bf09df74f5aa89a675a58
Vlad Kisialeuski <75371184+Kisel-kisel@users.noreply.github.com>
2023-04-30 13:23:15 +0200

    Add files via upload

---
+++ projectMays/src/main/java/bymay/vlad/Main.java
@@ -0,0 +1,41 @@
+package bymay.vlad;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Organization person = new Organization();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите дату своего рождения в формате: 1900-12-12");
+        String date = scanner.next();
+        LocalDate localDate = LocalDate.now();
+        LocalDate localDate1 = LocalDate.parse(date);
+        int years = localDate.getYear() - localDate1.getYear();
+        System.out.println(years);
+
+        List<Boolean> allOrgans = new ArrayList<>();
+
+        Boolean zero = person.youngs(years);
+        Boolean first = person.notYoungers(years);
+        Boolean second = person.workAge(years);
+        Boolean third = person.onlyOld(years);
+        allOrgans.add(zero);
+        allOrgans.add(first);
+        allOrgans.add(second);
+        allOrgans.add(third);;
+
+        FileWriter writer = new FileWriter("twoHomes.txt");
+
+        person.Organs(allOrgans, writer);
+
+        writer.close();
+
+
+
+    }
+
+}
